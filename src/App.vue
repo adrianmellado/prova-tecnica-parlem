b67d8f8dc9f4d1290712785d8db36cf22c20b3d1
Adrián Mellado <adrian99mellado@gmail.com>
2025-03-10 11:31:41 +0100

    maquetació de clients

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,30 @@
 </script>
 
 <template>
-  <h1>Clients</h1>
-  <div v-if="clients.length">
-    <div v-for="client in clients" :key="client.id" class="client">
-      <h2>{{ client.givenName }}</h2>
+  <div class="max-w-4xl mx-auto p-6">
+    <!-- Títol -->
+    <h1 class="text-3xl font-bold text-yellow-400 mb-4">
+      Clients <span class="text-black">telecom</span>
+    </h1>
+    <div v-if="clients.length">
+      <!-- Targeta client -->
+      <div v-for="client in clients" :key="client._id" class="bg-white shadow-md rounded-lg p-6 mb-6 border border-gray-200">
+        <div class="flex justify-between items-center">
+          <div>
+            <h2 class="text-xl font-semibold text-gray-800">{{ client.givenName }} {{ client.familyName1 }}</h2>
+            <p class="text-gray-500">{{ client.email }}</p>
+            <p class="text-gray-500">{{ client.phone }}</p>
+            <p class="text-gray-400 text-sm">{{ client.docType.toUpperCase() }}: {{ client.docNum }}</p>
+          </div>
+        </div>
+
+      </div>
     </div>
-  </div>
-  <div v-else>
-    <p>No hi ha clients disponibles.</p>
+
+    <!-- Si no hi ha clients -->
+    <div v-else class="text-gray-500 text-center mt-10">
+      <p>No hi ha clients disponibles</p>
+    </div>
   </div>
 </template>
 
