a5eb895d2bd27c7afefbdea17148f2e24024d0ec
Adrián Mellado <adrian99mellado@gmail.com>
2025-03-10 13:16:16 +0100

    to production

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  base: 'https://adrianmellado.github.io/prova-tecnica-parlem',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
